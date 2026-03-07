2f6f6341107cc69fe391dfa804de74209bf5b830
Andrey Vasilets <vasiletsandreas@yandex.ru>
2026-03-08 00:33:02 +0300

    10й проект v5

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -9,6 +9,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+
 @Slf4j
 @RestController
 @RequestMapping("/films")
